2961129d11cf3de64df92afd482fa1c67e129c08
Jorge-sudo <114958853+JorgeAronDelg@users.noreply.github.com>
2022-10-13 11:24:10 -0400

    Cambio Inicial

---
+++ README.md
@@ -0,0 +1,12 @@
+# AnimalAbstract
+## Descripción
+***
+Ejemplo de conexión IntelliJ + GitHub
+## Tecnologías
+***
+*[IntelliJ](https://www.jetbrains.com/)
+*[Java]
+## Colaboración
+***
+## FAQ
+***
---
+++ src/Gato.java
@@ -0,0 +1,19 @@
+public class Gato extends Anmal {
+        private String raza;
+
+        public Gato(String nombre,String raza) {
+            super(nombre);
+            this.raza = raza;
+        }
+        @Override
+        public void sonido(){
+            System.out.println(this.getNombre()+" dice: MIAU");
+        }
+
+        public void prueba(){
+            System.out.println("HOla prueba");
+        }
+
+
+}
+
